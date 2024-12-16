faba1fabd3d70cb4a87481c77dc8733483bd0563
Muhammad Shabir <n48jomzr@students.codeinstitute.net>
2024-12-16 17:28:43 +0000

    Add new secret key to env.py and replace existing key in settings.py

--- cedar_and_flame/settings.py
+++ cedar_and_flame/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q(!)#q-@!8v-ji20%1km@#pgsut1mu03)szbmbvw9sbi^hi!#k'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
