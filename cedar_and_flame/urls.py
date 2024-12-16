3eb7b2bb3ceda31849356d19cf7e45bdf997e71f
Muhammad Shabir <n48jomzr@students.codeinstitute.net>
2024-12-16 16:55:34 +0000

    create menu app and create basic view - link url to main project and test operation in local environment

--- cedar_and_flame/urls.py
+++ cedar_and_flame/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from menu import views as index_views
 
 urlpatterns = [
+    path('', index_views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
