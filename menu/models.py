de736623863032d5c3014f7214032f1b906be3f6
Muhammad Shabir <n48jomzr@students.codeinstitute.net>
2024-12-17 15:51:25 +0000

    Add doc string to define purpose/function of MenuItem model

--- menu/models.py
+++ menu/models.py
@@ -4,9 +4,25 @@ from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
+
 class MenuItem(models.Model):
     """
+    Represents a menu item in a restaurant or food service application.
 
+    This model stores the details of a menu item, including its name, description, 
+    associated image, category (e.g., starters, mains, sides, desserts, drinks), 
+    price, and availability status.
+
+    Attributes:
+        name (str): The name of the menu item.
+        description (str): A detailed description of the menu item.
+        image (CloudinaryField): An image associated with the menu item, stored in Cloudinary.
+        category (str): The category of the menu item, chosen from predefined categories (e.g., 'starters', 'mains', etc.).
+        price (Decimal): The price of the menu item.
+        is_available (bool): A boolean indicating whether the item is available for ordering.
+
+    Methods:
+        __str__(): Returns the name of the menu item.
     """
     CATEGORY_CHOICES = [
         ('starters', 'Starters'),
@@ -15,7 +31,7 @@ class MenuItem(models.Model):
         ('desserts', 'Desserts'),
         ('drinks', 'Drinks'),
     ]
-    
+
     name = models.CharField(max_length=200)
     description = models.TextField()
     image = CloudinaryField('image', default='placeholder')
