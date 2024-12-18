8dd6ebe1549b5d4ae5d84a104a7a2a89bf1f2b10
Muhammad Shabir <n48jomzr@students.codeinstitute.net>
2024-12-18 10:20:19 +0000

    Create menu.html in menu app and add simple url and views - add links to base.html and button in index.html - test to ensure view is dunctional

--- menu/urls.py
+++ menu/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+    path('menu/', views.menu, name='menu'),
     path('', views.homepage, name='home'),
 ]
--- menu/views.py
+++ menu/views.py
@@ -4,3 +4,7 @@ from django.views.generic import TemplateView
 def homepage(request):
 
     return render(request, "menu/index.html")
+
+def menu(request):
+
+    return render(request, "menu/menu.html")
