b740e4c45475e42075a4728472e16cf0f0f76c12
Muhammad Shabir <n48jomzr@students.codeinstitute.net>
2024-12-18 15:36:24 +0000

    Add post functionality to view, for UX add alert message upon successful submission - test menu item is added successfully

--- menu/views.py
+++ menu/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import TemplateView
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
 from .models import MenuItem
 from .forms import MenuItemForm
 
@@ -28,9 +28,21 @@ def menu(request):
         # Get menu items for the current category
         category_items = menu_items.filter(category=category_value)
         categorised_items.append((category_label, category_items))
+    
+    # Handle Post request from Menu Item Form
+    if request.method == "POST":
+        menu_item_form = MenuItemForm(data=request.POST)
+        if menu_item_form.is_valid():
+            menu_item = menu_item_form.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'New Menu Item Created'
+            )
+            # Redirect to menu page
+            return redirect('menu')
 
     # Display form
-    menu_item_form = MenuItemForm
+    menu_item_form = MenuItemForm()
 
     context = {
         'categorised_items': categorised_items,
