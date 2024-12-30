146c1d119dc3c2ed637e3a2b71ce14ef94e3e0d2
Muhammad Shabir <n48jomzr@students.codeinstitute.net>
2024-12-30 18:03:50 +0000

    Create basic function view and template for booking app - add links in base.html and index.html buttons - test

--- booking/views.py
+++ booking/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.http import Http404, HttpResponseRedirect
+from .models import Table, Booking
 
 # Create your views here.
 def booking(request):
-    return HttpResponse("This is the booking page")
+    """
+
+    """
+    return render(request, "booking/booking.html")
