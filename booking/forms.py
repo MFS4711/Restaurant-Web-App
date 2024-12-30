b565e0567a3030bed04e3d1a087aa6f9514ec9c2
Muhammad Shabir <n48jomzr@students.codeinstitute.net>
2024-12-30 18:52:10 +0000

    Add initial booking form display in booking app - Add wisgets for date and time selectors - Add incrementer for number of people selector - display in booking.html

---
+++ booking/forms.py
@@ -0,0 +1,47 @@
+from django import forms
+from django.utils import timezone
+from .models import Table, Booking
+
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Booking
+        fields = ['date', 'time', 'number_of_people', 'additional_notes',]
+
+    # Custom widget for the date field
+    date = forms.DateField(
+        widget=forms.DateInput(
+            format='%Y-%m-%d',  # Format to match the HTML5 date input format
+            attrs={
+                'type': 'date',
+                'class': 'form-control',
+                'min': timezone.now().date().strftime('%Y-%m-%d'),  # Disable past dates
+            }
+        )
+    )
+    
+    # Custom widget for the time field
+    time = forms.TimeField(
+        widget=forms.TimeInput(
+            format='%H:%M',  # Format to match the HTML5 time input format
+            attrs={
+                'type': 'time', 
+                'class': 'form-control',
+                'step': 900,
+            }
+        )
+    )
+
+    # Custom widget for the number of people field
+    number_of_people = forms.IntegerField(
+        min_value=1,  # Minimum value of 0
+        max_value=12,  # Maximum value of 12
+        widget=forms.NumberInput(
+            attrs={
+                'type': 'number',  # HTML5 number input type
+                'class': 'form-control',
+                'min': 1,  
+                'max': 12,  # Max to match max table
+                'step': 1,  # Increment by 1
+            }
+        )
+    )
--- booking/views.py
+++ booking/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from django.http import Http404, HttpResponseRedirect
 from .models import Table, Booking
+from .forms import BookingForm
 
 # Create your views here.
 def booking(request):
     """
 
     """
-    return render(request, "booking/booking.html")
+
+    # Display form
+    booking_form = BookingForm()
+
+    context = {
+        'booking_form': booking_form
+    }
+    
+    return render(request, "booking/booking.html", context)
