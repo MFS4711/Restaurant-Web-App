c238c03a4696dc2afd48d55d81fd278c00cd6df9
Muhammad Shabir <n48jomzr@students.codeinstitute.net>
2025-01-10 10:39:34 +0000

    Code review - Dashboard app = organise imports and add structured and meaningful comments to urls and views in app

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # View to display the admin dashboard
     path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
+
+    # View to display the customer dashboard for a specific user
     path('customer-dashboard/<int:user_id>', views.customer_dashboard, name='customer_dashboard'),
+
+    # View to display the staff dashboard
     path('staff-dashboard/', views.staff_dashboard, name='staff_dashboard'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,45 +1,64 @@
-from django.shortcuts import render, get_object_or_404, redirect, reverse
-from django.contrib.auth.decorators import login_required, user_passes_test
+from datetime import timedelta
+
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.models import User
 from django.db.models import Avg, Sum
-from django.http import Http404, HttpResponseRedirect
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from django.utils.timezone import localdate
-from datetime import datetime, timedelta
-from django.contrib.auth.models import User
-from booking.models import Table, Booking
+
 from booking.forms import BookingFilterForm
-from booking.utils import generate_time_slots, is_table_available, generate_conflicting_time_range, get_table_availability_for_day
+from booking.models import Booking
+from booking.utils import generate_time_slots, get_table_availability_for_day
 
 # Create your views here.
+
 
 @login_required
 def customer_dashboard(request, user_id):
     """
+    Handles the customer dashboard view, displaying a list of upcoming, past, 
+    and customer action-required bookings for a specific user.
 
+    **Context:**
+    - user: The user whose dashboard is being viewed.
+    - upcoming_bookings: Bookings for the user that are in the future.
+    - past_bookings: Completed bookings for the user in the past.
+    - customer_action_required_bookings: Bookings requiring customer confirmation.
+
+    **Template:**
+    :template:`dashboard/customer_dashboard.html`
     """
-    # Check if the logged-in user is the same as the user_id in the URL
+    # Ensure the logged-in user matches the user_id in the URL
     if request.user.id != int(user_id):
-        # If they are not the same, redirect to a different page, for example, a 404 or the homepage
+        # If they don't match, raise a 404 error to prevent unauthorized access
         raise Http404("You are not authorized to view this page.")
-    
+
+    # Retrieve the user object, ensuring it exists in the database
     user = get_object_or_404(User, id=user_id)
 
-    # Get current date and time for comparisons
+    # Get the current time to filter bookings accordingly
     current_time = timezone.now()
 
-    # Fetch upcoming bookings (bookings that are in the future)
-    upcoming_bookings = Booking.objects.filter(user=user, date__gte=current_time.date(
-    ), time__gte=current_time.time(), status__in=['confirmed', 'pending']).order_by('date', 'time')
+    # Fetch upcoming bookings (those scheduled in the future)
+    upcoming_bookings = Booking.objects.filter(
+        user=user, date__gte=current_time.date(), time__gte=current_time.time(),
+        status__in=['confirmed', 'pending']
+    ).order_by('date', 'time')
 
-    # Fetch past bookings (bookings that are in the past)
-    past_bookings = Booking.objects.filter(user=user, date__lt=current_time.date(
-    ), status='completed').order_by('-date', '-time')
+    # Fetch past bookings (those already completed)
+    past_bookings = Booking.objects.filter(
+        user=user, date__lt=current_time.date(), status='completed'
+    ).order_by('-date', '-time')
 
-    # Fetch customer action required bookings (status 'customer_confirmation_required')
+    # Fetch bookings requiring customer confirmation
     customer_action_required_bookings = Booking.objects.filter(
-        user=user, status=Booking.CUSTOMER_CONFIRMATION_REQUIRED).order_by('date', 'time')
+        user=user, status=Booking.CUSTOMER_CONFIRMATION_REQUIRED
+    ).order_by('date', 'time')
 
+    # Pass all relevant data to the template context for rendering
     context = {
         'user': user,
         'upcoming_bookings': upcoming_bookings,
@@ -51,29 +70,48 @@ def customer_dashboard(request, user_id):
 
 # Custom decorator to check if the user is a staff member
 def is_staff(user):
+    """
+    Custom function to check if the user has staff permissions.
+
+    **Returns:** 
+    - True if the user is a staff member, else False.
+    """
     return user.is_staff
 
-@login_required(login_url='/login/')  # redirect to login page if not authenticated
+
+# Redirect to login if the user is not authenticated
+@login_required(login_url='/login/')
+# Ensure the user is a staff member
 @user_passes_test(is_staff, login_url='/unauthorized/')
 def staff_dashboard(request):
     """
+    Handles the staff dashboard view, displaying table availability for today, 
+    and the bookings scheduled for today.
 
+    **Context:**
+    - time_slots: Generated time slots for the day.
+    - table_availability: Availability of tables based on generated time slots.
+    - today: Today's date.
+    - today_bookings: Bookings for today that are confirmed.
+
+    **Template:**
+    :template:`dashboard/staff_dashboard.html`
     """
-    # Use the utility function to generate time slots with dynamic opening hours
+    # Generate time slots with dynamic opening hours, in this case, 15-minute intervals
     time_slots = generate_time_slots(interval_minutes=15)
 
-    # Get today's date
+    # Get today's date for scheduling and availability checks
     today = timezone.now().date()
 
-    # Get table availability using the utility function
+    # Check table availability for the current day using the generated time slots
     table_availability = get_table_availability_for_day(today, time_slots)
 
-    # Fetch bookings for today (Confirmed and Pending)
+    # Fetch confirmed bookings for today
     today_bookings = Booking.objects.filter(
         date=today, status__in=[Booking.CONFIRMED]
     ).order_by('time')
 
-    # Pass data to the context
+    # Pass all the data to the template for rendering
     context = {
         'time_slots': time_slots,
         'table_availability': table_availability,
@@ -84,57 +122,91 @@ def staff_dashboard(request):
     return render(request, 'dashboard/staff_dashboard.html', context)
 
 
-# Custom decorator to check if the user is a staff member
+# Custom decorator to check if the user is a superuser (admin)
 def is_superuser(user):
+    """
+    Custom function to check if the user is a superuser (admin).
+
+    **Returns:** 
+    - True if the user is a superuser, else False.
+    """
     return user.is_superuser
 
 
-@login_required(login_url='/login/')  # redirect to login page if not authenticated
+# Redirect to login if the user is not authenticated
+@login_required(login_url='/login/')
+# Ensure the user is a superuser (admin)
 @user_passes_test(is_superuser, login_url='/unauthorized/')
 def admin_dashboard(request):
     """
+    Handles the admin dashboard view, providing an overview of booking statistics 
+    based on a filter range for booking dates. Displays the total number of bookings 
+    and various statistics on booking status and sizes.
 
+    **Context:**
+    - filter_form: The form used for filtering bookings by date range.
+    - bookings: The bookings within the filtered date range.
+    - total_bookings: The total count of bookings within the date range.
+    - total_confirmed: The total number of confirmed bookings.
+    - total_pending: The total number of pending bookings.
+    - total_cancelled: The total number of cancelled bookings.
+    - total_no_show: The total number of no-show bookings.
+    - start_date: The start date of the filter range.
+    - end_date: The end date of the filter range.
+    - avg_booking_size: The average number of people per booking.
+    - avg_bookings_per_day: The average number of bookings per day in the date range.
+    - avg_visitors_per_day: The average number of visitors per day in the date range.
+
+    **Template:**
+    :template:`dashboard/admin_dashboard.html`
     """
-    # Get today's date
+    # Get today's date for use in filtering and statistics calculations
     today = timezone.now().date()
 
-    # Initialize the filter form
+    # Initialize the filter form to allow the admin to filter bookings by date range
     filter_form = BookingFilterForm(request.GET)
 
-    # Set default dates (today) for the view if no form submission
+    # Set default dates to today in case the filter form is not submitted
     start_date = end_date = today
 
-    # Apply filter logic
+    # If the filter form is valid, apply the filtered date range
     if filter_form.is_valid():
-        # Get the date range based on selected filter
         start_date, end_date = filter_form.get_filtered_date_range()
 
-    # Fetch bookings based on the filtered date range
+    # Fetch bookings that fall within the specified date range
     bookings = Booking.objects.filter(date__range=[start_date, end_date])
 
-    # Calculate booking statistics
-    total_bookings = bookings.count()
-    total_confirmed = bookings.filter(status=Booking.CONFIRMED).count()
-    total_pending = bookings.filter(status=Booking.PENDING).count()
-    total_cancelled = bookings.filter(status=Booking.CANCELLED).count()
-    total_no_show = bookings.filter(status=Booking.NO_SHOW).count()
+    # Calculate various statistics for bookings in the selected date range
+    total_bookings = bookings.count()  # Total number of bookings
+    total_confirmed = bookings.filter(
+        status=Booking.CONFIRMED).count()  # Total confirmed bookings
+    total_pending = bookings.filter(
+        status=Booking.PENDING).count()  # Total pending bookings
+    total_cancelled = bookings.filter(
+        status=Booking.CANCELLED).count()  # Total cancelled bookings
+    total_no_show = bookings.filter(
+        status=Booking.NO_SHOW).count()  # Total no-show bookings
 
-    # Calculate overall statistics
+    # Calculate averages if there are bookings in the selected date range
     if total_bookings > 0:
-        # Average Booking Size (total number of people / total bookings)
-        avg_booking_size = bookings.aggregate(Avg('number_of_people'))['number_of_people__avg']
-        
-        # Average Bookings Per Day (total bookings / number of days in range)
-        num_days = (end_date - start_date).days + 1  # +1 to include both start and end days
+        # Average booking size: total number of people / total bookings
+        avg_booking_size = bookings.aggregate(Avg('number_of_people'))[
+            'number_of_people__avg']
+
+        # Average bookings per day: total bookings / number of days in the range
+        # +1 to include both start and end days
+        num_days = (end_date - start_date).days + 1
         avg_bookings_per_day = total_bookings / num_days
 
-        # Average Number of Visitors per Day (total number of people / number of days in range)
-        total_visitors = bookings.aggregate(Sum('number_of_people'))['number_of_people__sum']
+        # Average visitors per day: total number of visitors / number of days in the range
+        total_visitors = bookings.aggregate(Sum('number_of_people'))[
+            'number_of_people__sum']
         avg_visitors_per_day = total_visitors / num_days if total_visitors else 0
     else:
+        # If no bookings, set averages to 0
         avg_booking_size = avg_bookings_per_day = avg_visitors_per_day = 0
 
-    # Context for template rendering
+    # Pass all relevant data to the context for rendering the admin dashboard template
     context = {
         'filter_form': filter_form,
         'bookings': bookings,
