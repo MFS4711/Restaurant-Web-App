c4f82070f0a16a16df0ac691bf12f2e23530e3eb
Muhammad Shabir <n48jomzr@students.codeinstitute.net>
2025-01-12 10:11:43 +0000

    Menu App - Import for login_required added

--- menu/views.py
+++ menu/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import MenuItem
 from .forms import MenuItemForm
